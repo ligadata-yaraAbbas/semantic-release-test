b36ec026337f89b5c18e439205f13b421c8acce6
ligadata-yaraAbbas <134509741+ligadata-yaraAbbas@users.noreply.github.com>
2025-02-17 18:10:51 +0300

    Update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-  };
-  
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'body-max-line-length': [0, 'always', 120] // Allows up to 120 characters per line
+  }
+};
